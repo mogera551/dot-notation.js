7d912f79b7e6379c7214c5150fdda73401f6af1c
mogera551 <mogera551@gmail.com>
2023-11-11 07:33:12 +0900

    [readonly]PropertyName

--- src/PropertyName.js
+++ src/PropertyName.js
@@ -1,68 +1,114 @@
 import { WILDCARD, DELIMITER } from "./Const.js";
 
 export class PropertyName {
-  /**
-   * @type {string}
-   */
-  name;
-  /**
-   * @type {string[]}
-   */
-  pathNames = [];
-  /**
-   * @type {string[]}
-   */
-  parentPathNames = [];
-  /**
-   * @type {string}
-   */
-  parentPath;
-  /**
-   * @type {string[]}
-   */
-  parentPaths = [];
-  /**
-   * @type {Set<string>}
-   */
-  setOfParentPaths;
-  /**
-   * @type {RegExp}
-   */
-  regexp;
-  /**
-   * @type {number}
-   */
-  level = 0;
-  /**
-   * @type {boolean}
-   */
-  isPrimitive;
+  /** @type {string} */
+  #name;
+  get name() {
+    return this.#name;
+  }
+
+  /** @type {string[]} */
+  #pathNames;
+  /** @type {string[]} 名前（name）をドットで区切った配列 */
+  get pathNames() {
+    return this.#pathNames;
+  }
+
+  /** @type {string[] */
+  #parentPathNames;
+  /** @type {string[]} 名前（name）をドットで区切った配列、最後の要素を含まない */
+  get parentPathNames() {
+    return this.#parentPathNames;
+  }
+
+  /** @type {string} */
+  #parentPath;
+  /** @type {string} 親の名前、親名前配列（parentPathNames）をjoinしたもの */
+  get parentPath() {
+    return this.#parentPath;
+  }
+
+  /** @type {string[]} */
+  #parentPaths;
+  /** @type {string[]} 親の名前候補すべて */
+  get parentPaths() {
+    return this.#parentPaths;
+  }
+
+  /** @type {Set<string>} */
+  #setOfParentPaths;
+  /** @type {Set<string>} 親の名前候補のセット */
+  get setOfParentPaths() {
+    return this.#setOfParentPaths;
+  }
+
+  /** @type {string} */
+  #lastPathName;
+  /** @type {string} 名前（name）をドットで区切った配列の最後の要素 */
+  get lastPathName() {
+    return this.#lastPathName;
+  }
+
+  /** @type {RegExp} */
+  #regexp;
+  /** @type {RegExp} ドット記法の書式が一致するかテストするための正規表現 */
+  get regexp() {
+    return this.#regexp;
+  }
+
+  /** @type {number} */
+  #level;
+  /** @type {number} ループレベル、名前（name）に含むワイルドカード（*）の数 */
+  get level() {
+    return this.#level;
+  }
+
+  /** @type {boolean} */
+  #isPrimitive;
+  /** @type {boolean} プリミティブかどうか、名前（name）にドット（.）を含まない */
+  get isPrimitive() {
+    return this.#isPrimitive;
+  }
+
+  /** @type {string} */
+  #nearestWildcardName;
+  /** @type {string}  最後のワイルドカードまでの部分 */
+  get nearestWildcardName() {
+    return this.#nearestWildcardName;
+  }
+
+  /** @type {string} */
+  #nearestWildcardParentName;
+  /** @type {string}  最後のワイルドカードまでの部分の親 */
+  get nearestWildcardParentName() {
+    return this.#nearestWildcardParentName;
+  }
 
   /**
    * 
-   * @param {string} name 
+   * @param {string} name プロパティ名
    */
   constructor(name) {
-    this.name = name;
-    this.pathNames = name.split(DELIMITER);
-    this.parentPathNames = this.pathNames.slice(0, -1);
-    this.parentPaths = this.parentPathNames.reduce((paths, pathName) => { 
+    this.#name = name;
+    this.#pathNames = name.split(DELIMITER);
+    this.#parentPathNames = this.#pathNames.slice(0, -1);
+    this.#parentPaths = this.#parentPathNames.reduce((paths, pathName) => { 
       paths.push(paths.at(-1)?.concat(pathName) ?? [pathName]);
       return paths;
     }, []).map(paths => paths.join("."));
-    this.setOfParentPaths = new Set(this.parentPaths);
-    this.parentPath = this.parentPathNames.join(DELIMITER);
-    this.lastPathName = this.pathNames.at(-1);
-    this.regexp = new RegExp("^" + name.replaceAll(".", "\\.").replaceAll("*", "([0-9a-zA-Z_]*)") + "$");
-    this.level = this.pathNames.filter(pathName => pathName === WILDCARD).length;
-    this.isPrimitive = (this.pathNames.length === 1);
-    this.nearestWildcardName = undefined;
-    this.nearestWildcardParentName = undefined;
-    if (this.level > 0) {
-      for(let i = this.pathNames.length - 1; i >= 0; i--) {
-        if (this.pathNames[i] === WILDCARD) {
-          this.nearestWildcardName = this.pathNames.slice(0, i + 1).join(".");
-          this.nearestWildcardParentName = this.pathNames.slice(0, i).join(".");
+    this.#setOfParentPaths = new Set(this.#parentPaths);
+    this.#parentPath = this.#parentPathNames.join(DELIMITER);
+    this.#lastPathName = this.#pathNames.at(-1);
+    this.#regexp = new RegExp("^" + name.replaceAll(".", "\\.").replaceAll("*", "([0-9a-zA-Z_]*)") + "$");
+    this.#level = this.#pathNames.reduce((level, pathName) => level += (pathName === WILDCARD ? 1 : 0), 0);
+    this.#isPrimitive = (this.#pathNames.length === 1);
+    this.#nearestWildcardName = undefined;
+    this.#nearestWildcardParentName = undefined;
+    if (this.#level > 0) {
+      for(let i = this.#pathNames.length - 1; i >= 0; i--) {
+        if (this.#pathNames[i] === WILDCARD) {
+          this.#nearestWildcardName = this.#pathNames.slice(0, i + 1).join(".");
+          this.#nearestWildcardParentName = this.#pathNames.slice(0, i).join(".");
           break;
         }
       }
@@ -76,7 +122,7 @@ export class PropertyName {
    */
   static create(name) {
     const propertyName = this.propertyNameByName.get(name);
-    if (propertyName) return propertyName;
+    if (typeof propertyName !== "undefined") return propertyName;
     const newPropertyName = new PropertyName(name);
     this.propertyNameByName.set(name, newPropertyName);
     return newPropertyName;
